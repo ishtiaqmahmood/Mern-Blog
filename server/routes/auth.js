72b008c958fe73506e520a039b4ff580d41fc1f4
ishtiaqmahmood <ishtiaqmahmood45@gmail.com>
2022-06-21 16:48:42 +0600

    Authorization

---
+++ server/routes/auth.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin,
+} = require("../controllers/auth");
+
+const router = express.Router();
+
+// validators
+const { runValidation } = require("../validators/index");
+const {
+  userSignupValidator,
+  userSigninValidator,
+} = require("../validators/auth");
+
+router.post("/signup", userSignupValidator, runValidation, signup);
+router.post("/signin", userSigninValidator, runValidation, signin);
+router.get("/signout", signout);
+router.get("/secret", requireSignin, (req, res) => {
+  res.json({
+    message: "you have access to secret page.",
+  });
+});
+
+module.exports = router;
